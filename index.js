e0ce31161158129a94dded713e02012448521d8c
DeeKahy <97156953+DeeKahy@users.noreply.github.com>
2023-02-06 10:57:00 +0100

    init
    Co-authored-by: mantarias <mantarias@users.noreply.github.com>
    Co-authored-by: CarlHejlesen <CarlHejlesen@users.noreply.github.com>

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+app.use(express.static("public"));
+
+app.get('/', (req, res) => {
+  res.sendFile('public/index.html', {root: __dirname })
+});
+
+app.listen(3000, () => {
+  console.log('Example app listening on http://localhost:3000/');
+});
